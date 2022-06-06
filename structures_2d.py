f146b358235a851a0db05f4aff5842dd85bd9783
ArchTangent <95271114+ArchTangent@users.noreply.github.com>
2022-06-06 15:50:13 -0700

    Add Tile and Point testing

--- structures_2d.py
+++ structures_2d.py
@@ -1,5 +1,94 @@
 # Structures for 2D FOV
 
-from dataclasses import dataclass
+from enum import Enum
+
+class Structure(Enum):
+    """2D tilemap structure that blocks sight and/or movement.
+    
+    Structures are placed in the *center* of the tile, unlike Walls, which
+    are on the tile edges.
+    """
+    ERROR = -1  # Invalid structure type
+    NONE = 0    # No structure
+    FULL = 1    # Fills entire tile in x, y, and z directions
+    WIDE = 2    # Fills tile halfway along x, y; fully in z
+    THIN = 3    # Fills tile 1/4 of the way along x, y; fully in z
+
+    @staticmethod
+    def from_str(name: str):
+        """Creates an instance from a (JSON, YAML, TOML) string."""
+        match name.upper():
+            case "NONE":
+                return Structure.NONE
+            case "FULL":
+                return Structure.FULL
+            case "WIDE":
+                return Structure.WIDE
+            case "THIN":
+                return Structure.THIN
+            case _:
+                print(f"Invalid Structure name `{name}`!")
+                return Structure.ERROR
 
 
+class Wall(Enum):
+    """2D tilemap structure that blocks sight and/or movement along edges.
+    
+    Walls are placed on the *edges* of a tile.
+    """
+    ERROR = -1  # Invalid wall type
+    NONE = 0    # No wall
+    FULL = 1    # Full 
+    HALF = 2    # Half height wall along a given edge
+    WINDOW = 3  # Allows sight, but blocks movement along a given edge
+
+    @staticmethod
+    def from_str(name: str):
+        """Creates an instance from a (JSON, YAML, TOML) string."""
+        match name.upper():
+            case "NONE":
+                return Wall.NONE
+            case "FULL":
+                return Wall.FULL
+            case "HALF":
+                return Wall.HALF
+            case "WINDOW":
+                return Wall.WINDOW
+            case _:
+                print(f"Invalid Wall name `{name}`!")
+                return Wall.ERROR 
+
+
+def test_structure_str():
+    suite = [
+        ("none", Structure.NONE),
+        ("NONE", Structure.NONE),
+        ("full", Structure.FULL),
+        ("FULL", Structure.FULL),
+        ("wide", Structure.WIDE),
+        ("WIDE", Structure.WIDE),
+        ("thin", Structure.THIN),
+        ("THIN", Structure.THIN),
+        ("abcd", Structure.ERROR),
+        ("WXYZ", Structure.ERROR),
+    ]
+    for input, expected in suite:
+        actual = Structure.from_str(input)
+        assert actual == expected
+
+def test_wall_str():
+    suite = [
+        ("none", Wall.NONE),
+        ("NONE", Wall.NONE),
+        ("full", Wall.FULL),
+        ("FULL", Wall.FULL),
+        ("half", Wall.HALF),
+        ("HALF", Wall.HALF),
+        ("window", Wall.WINDOW),
+        ("WINDOW", Wall.WINDOW),
+        ("efgh", Wall.ERROR),
+        ("STUV", Wall.ERROR),
+    ]
+    for input, expected in suite:
+        actual = Wall.from_str(input)
+        assert actual == expected
--- tilemap_2d.py
+++ tilemap_2d.py
@@ -1,9 +1,9 @@
 # Tilemap and Tiles for 2D FOV
 
-# from __future__ import annotations
 from dataclasses import dataclass
-from collections import namedtuple
 from typing import List, Dict, Tuple, Optional
+from structures_2d import Structure
+import json
 
 @dataclass
 class TileId:
@@ -17,6 +17,10 @@ class TileId:
     
         return Point2d(x, y)
 
+    def in_bounds(self, num_tiles) -> bool:
+        """Returns `True` if Tile ID is in bounds based on # of tiles in tilemap."""
+        return 0 < self.val < num_tiles
+
 @dataclass
 class Point2d:
     __slots__ = ["x", "y"]
@@ -26,6 +30,10 @@ class Point2d:
     def to_tile_id(self, xdims: int):
         """Converts a 2D point into a tile ID based on tilemap's X dimensions."""
         return TileId(self.x + self.y * xdims)
+
+    def in_bounds(self, xdims, ydims) -> bool:
+        """Returns `True` if point is in bounds based on tilemap's dimensions."""
+        return self.x > -1 and self.x < xdims and self.y > -1 and self.y < ydims
 
 @dataclass
 class Tile:
@@ -39,17 +47,75 @@ class Tile:
 
 class Tilemap2d:
     def __init__(
-        self, xdims: int, ydims: int, structures: Optional[Dict[Tuple[int, int], int]]
+        self, xdims: int, ydims: int, tiles: Optional[Dict[Tuple, List]]
     ) -> None:
+        """Builds map from {(x,y):[struct, n_wall, s_wall, e_wall, w_wall]} data."""
         self.xdims = xdims
         self.ydims = ydims
         self.tiles: List[Tile] = []
+        # TODO: error check tiles in bounds
 
-
-class TilemapBuilder2d:
     @staticmethod
-    def empty(xdims, ydims) -> Tilemap2d:
+    def from_json(data: str):
+        """Create a tilemap from (JSON) string of tilemap data.
+        
+        Keys:
+        - "dims": (x,y) dimensions.
+        - "tiles": {(x,y): [struct, n_wall, s_wall, e_wall, w_wall]} tile data.
+        """
+        json_dict = json.loads(data)
+
+        if not map_data_valid(json_dict):
+            return Tilemap2d(10, 10, None)
+
+        xdims, ydims = json_dict["dims"]
+        tiles = json_dict["tiles"]
+
+        return Tilemap2d(xdims, ydims, tiles)
+
+    @staticmethod
+    def empty(xdims: int, ydims: int):
         return Tilemap2d(xdims, ydims, None)
+
+
+def map_data_valid(json_dict: Dict) -> bool:
+    """Returns `True` if map data is in {(int, int): [int, int, int, int] format."""
+    valid = True
+
+    dims = json_dict.get("dims", None)
+    if not dims:
+        print(f"ERROR: Missing JSON dimension data! Expected (int, int)")
+        return False
+    if not valid_2d_coords(dims):
+        print(f"ERROR: Malformed JSON dimension data! Expected (int, int)")
+        return False
+
+    tiles = json_dict.get("tiles", {})
+    for coords, tile_data in tiles:
+        if not valid_2d_coords(coords):
+            print(f"ERROR: Malformed JSON coordinate data! Expected (int, int)")
+            return False
+        if not valid_tile_data(tile_data):
+            print(f"ERROR: Malformed JSON tile data! Expected [int, int, int, int int]")
+            return False
+
+    return valid
+
+def valid_2d_coords(value: Tuple) -> bool:
+    """Returns True if value is an (int, int) 2-tuple.  Does not check bounds."""
+    return (
+        type(value) == tuple and len(value) == 2 and 
+        type(value[0]) == int and type(value[1]) == int
+    )
+
+def valid_tile_data(value: List) -> bool:
+    """Returns True if value is a [int, int, int, int, int] list."""
+    return (
+        type(value) == list and len(value) == 5 and 
+        type(value[0]) == int and type(value[1]) == int and
+        type(value[2]) == int and type(value[3]) == int and
+        type(value[4]) == int
+    )
 
 
 def test_point_to_tile():
@@ -69,6 +135,20 @@ def test_point_to_tile():
 
         assert tile_id == TileId(expected)
 
+def test_point_in_bounds():
+    xdims, ydims = (10, 10)
+    suite = [
+        (-1,5), (5,-1), (10,5), (5,10), (20,0), 
+        (0,0), (1,0), (0,1), (9,1), (1,9)
+    ]
+    results = [
+        False, False, False, False, False,
+        True, True, True, True, True 
+    ]
+    for xy, expected in zip(suite, results):
+        point = Point2d(*xy)
+        assert point.in_bounds(xdims, ydims) == expected
+
 def test_tile_to_point():
     """Note: conversion does *not* check for out-of-bounds tiles."""
     suite = [
@@ -85,6 +165,23 @@ def test_tile_to_point():
         point = tile_id.to_point(xdims)
 
         assert point == Point2d(*expected)
+        
+def test_tile_in_bounds():
+    xdims, ydims = (10, 10)
+    num_tiles = xdims * ydims
+    suite = [
+        # TODO: fix negative numbers:  need to be OOB (-5,1) -> tile 49 which
+        # TODO:      is in bounds, but shouldn't be!
+        (-1,5), (5,-1), (10,5), (5,10), (20,0), 
+        (0,0), (1,0), (0,1), (9,1), (1,9)
+    ]
+    results = [
+        False, False, False, False, False,
+        True, True, True, True, True 
+    ]
+    for xy, expected in zip(suite, results):
+        tile_id = Point2d(*xy).to_tile_id(xdims)
+        assert tile_id.in_bounds(num_tiles) == expected        
 
 if __name__ == "__main__":
     print("-----  Tilemap 2D -----\n")
