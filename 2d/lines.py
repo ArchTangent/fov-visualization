92a874fa20a0e89a8ba0a44b3f37a8a6870a2c86
ArchTangent <archtangent13@gmail.com>
2023-06-06 20:42:30 -0700

    Update `lines.py` and `lines_3d.py` with new lines

--- 2d/lines.py
+++ 2d/lines.py
@@ -3,7 +3,104 @@ from typing import List, Tuple
 
 
 def bresenham(x1: int, y1: int, x2: int, y2: int) -> List[Tuple[int, int]]:
-    """Bresenham's Line Algorithm - 2D version.
+    """Breshenham's line algorithm - 2D version."""
+    result = [(x1, y1)]
+
+    dx, dy = abs(x2 - x1), abs(y2 - y1)       
+    x_inc, y_inc = 1, 1
+    x, y = x1, y1
+
+    if x2 < x1:
+        x_inc = -1
+    if y2 < y1:
+        y_inc = -1
+
+    # Y-primary
+    if dy > dx:
+        tx = 2 * dx - dy
+
+        for _ in range(dy):
+            y += y_inc
+            if tx >= 0:
+                x += x_inc
+                tx -= 2 * dy            
+            tx += 2 * dx
+
+            result.append((x, y))
+
+    # X-primary
+    else:
+        ty = 2 * dy - dx
+
+        for _ in range(dx):
+            x += x_inc
+            if ty >= 0:
+                y += y_inc
+                ty -= 2 * dx            
+            ty += 2 * dy
+
+            result.append((x, y))
+
+    return result
+
+
+def bresenham_full(x1: int, y1: int, x2: int, y2: int) -> List[Tuple[int, int]]:
+    """Breshenham's line algorithm - 2D version that gets all tiles touched.
+    
+    `tx`, `ty` indicate full tile distance; `hx`, `hy` indicate half tile distance.
+    """
+    result = [(x1, y1)]
+
+    dx, dy = abs(x2 - x1), abs(y2 - y1)       
+    x_inc, y_inc = 1, 1
+    x, y = x1, y1
+
+    if x2 < x1:
+        x_inc = -1
+    if y2 < y1:
+        y_inc = -1
+
+    # Y-primary
+    if dy > dx:
+        tx = 2 * dx - dy
+
+        for _ in range(dy):
+            y += y_inc
+            hx = tx - dx
+
+            if hx >= 0:
+                result.append((x + x_inc, y - y_inc))
+            if hx <= 0:
+                result.append((x, y))            
+            if tx > 0:
+                x += x_inc
+                tx -= 2 * dy
+                result.append((x, y))
+            tx += 2 * dx
+
+    # X-primary
+    else:
+        ty = 2 * dy - dx
+
+        for _ in range(dx):
+            x += x_inc
+            hy = ty - dy
+
+            if hy >= 0:
+                result.append((x - x_inc, y + y_inc))
+            if hy <= 0:
+                result.append((x, y)) 
+            if ty > 0:
+                y += y_inc        
+                ty -= 2 * dx
+                result.append((x, y))
+            ty += 2 * dy
+
+    return result
+
+
+def fire_line(x1: int, y1: int, x2: int, y2: int) -> List[Tuple[int, int]]:
+    """Fire line - 2D version. Differs slightly from Breshenham.
 
     All lines should be reciprocal. Uses 1000 for granularity `g`."""
     # Deltas
@@ -61,10 +158,10 @@ def bresenham(x1: int, y1: int, x2: int, y2: int) -> List[Tuple[int, int]]:
     return result
 
 
-def bresenham_full(
+def fire_line_full(
     x1: int, y1: int, x2: int, y2: int, g: int = 1000
 ) -> List[Tuple[int, int]]:
-    """Bresenham's Line Algorithm - 2D version that gets all tiles touched.
+    """Fire line full - 2D version that gets all tiles touched.
 
     Secondary distance is th secondary axis distance from the next tile edge.
     Indicates a crossing of a tile boundary.
@@ -91,7 +188,7 @@ def bresenham_full(
 
     # Choose primary axis
     if abs_dy > abs_dx:
-        dxdy = int(abs_dx / abs_dy * g)
+        dxdy = int(abs_dx / abs_dy * g)        
         dxdy_edge = dxdy // 2
         dist = g // 2
 
@@ -109,7 +206,7 @@ def bresenham_full(
             if dist > g:
                 dist -= g
                 x += x_inc
-                result.append((x, y))
+                result.append((x, y))        
     else:
         dydx = int(abs_dy / abs_dx * g)
         dydx_edge = dydx // 2
@@ -139,18 +236,6 @@ def bresenham_full(
 #      ##     ######     ######      ##
 #      ##     ##              ##     ##
 #      ##     ########  #######      ##
-
-
-def test_bresenham_2D_reciprocal():
-    """Checks if 2D bresenham lines are reciprocal."""
-    suite = [(int(0), int(0), x, y) for x in range(0, 5) for y in range(-5, 5)]
-    suite.extend([(x, y, 0, 0) for x in range(0, 5) for y in range(-5, 5)])
-
-    for x1, y1, x2, y2 in suite:
-        fwd = bresenham(x1, y1, x2, y2)
-        rev = bresenham(x2, y2, x1, y1)
-        assert sorted(fwd) == sorted(rev)
-
 
 def test_bresenham_2D_full():
     """Check expected values for the `bresenham_full()` 2D function."""
@@ -183,5 +268,51 @@ def test_bresenham_2D_full():
         [(4, 2), (3, 2), (3, 1), (2, 1), (1, 1), (1, 0), (0, 0)],
     ]
     actual = [bresenham_full(*coords) for coords in suite]
+    for i, e in enumerate(expected):
+        assert actual[i] == e
+
+
+def test_fire_line_2D_reciprocal():
+    """Checks if 2D fire lines are reciprocal."""
+    suite = [(int(0), int(0), x, y) for x in range(0, 5) for y in range(-5, 5)]
+    suite.extend([(x, y, 0, 0) for x in range(0, 5) for y in range(-5, 5)])
+
+    for x1, y1, x2, y2 in suite:
+        fwd = fire_line(x1, y1, x2, y2)
+        rev = fire_line(x2, y2, x1, y1)
+        assert sorted(fwd) == sorted(rev)
+
+
+def test_fire_line_2D_full():
+    """Check expected values for the `fire_line_full()` 2D function."""
+    suite = [
+        (0, 0, 5, 0),
+        (0, 0, -5, 0),
+        (0, 0, 0, 5),
+        (0, 0, 0, -5),
+        (5, 0, 0, 0),
+        (-5, 0, 0, 0),
+        (0, 5, 0, 0),
+        (0, -5, 0, 0),
+        (0, 0, 2, 2),
+        (2, 2, 0, 0),
+        (0, 0, 4, 2),
+        (4, 2, 0, 0),
+    ]
+    expected = [
+        [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0)],
+        [(0, 0), (-1, 0), (-2, 0), (-3, 0), (-4, 0), (-5, 0)],
+        [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5)],
+        [(0, 0), (0, -1), (0, -2), (0, -3), (0, -4), (0, -5)],
+        [(5, 0), (4, 0), (3, 0), (2, 0), (1, 0), (0, 0)],
+        [(-5, 0), (-4, 0), (-3, 0), (-2, 0), (-1, 0), (0, 0)],
+        [(0, 5), (0, 4), (0, 3), (0, 2), (0, 1), (0, 0)],
+        [(0, -5), (0, -4), (0, -3), (0, -2), (0, -1), (0, 0)],
+        [(0, 0), (0, 1), (1, 0), (1, 1), (1, 2), (2, 1), (2, 2)],
+        [(2, 2), (2, 1), (1, 2), (1, 1), (1, 0), (0, 1), (0, 0)],
+        [(0, 0), (1, 0), (1, 1), (2, 1), (3, 1), (3, 2), (4, 2)],
+        [(4, 2), (3, 2), (3, 1), (2, 1), (1, 1), (1, 0), (0, 0)],
+    ]
+    actual = [fire_line_full(*coords) for coords in suite]
     for i, e in enumerate(expected):
         assert actual[i] == e
